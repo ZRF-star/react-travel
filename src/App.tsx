0b2a56c5d51dacaa3b33ec1276ae922f5a9afd9c
ZRF-star <zhangrf136@nenu.edu.cn>
2021-10-14 15:58:23 +0800

    refactor: 封装header 和 footer 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-// 用模块化的方式导入css
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import { Header, Footer } from "./components";
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <Header />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
