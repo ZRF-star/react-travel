8941a12bc88120451aad1298da2bdbd4da8d772b
ZRF-star <zhangrf136@nenu.edu.cn>
2021-10-18 11:29:00 +0800

    refactor: 文案国际化

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import styles from "./Header.module.css";
-import logo from '../../assets/logo.svg';
 import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
-import { GlobalOutlined } from "@ant-design/icons";
 import { withRouter,RouteComponentProps } from 'react-router-dom'
+import { withTranslation, WithTranslation } from 'react-i18next'
+import { GlobalOutlined } from "@ant-design/icons";
 import store from '../../redux/store'
 import { LanguageState } from '../../redux/languageReducer'
+import styles from "./Header.module.css";
+import logo from '../../assets/logo.svg';
 
 interface IHeaderProps {}
 interface State extends LanguageState {}
 
-type Props = IHeaderProps & RouteComponentProps;
+type Props = IHeaderProps & RouteComponentProps & WithTranslation;
 
 export class HeaderComponent extends React.Component<Props, State> {
     constructor(props) {
@@ -46,13 +47,13 @@ export class HeaderComponent extends React.Component<Props, State> {
     }
 
     render() {
-        const { history } = this.props;
+        const { history, t } = this.props;
         return (
             <div className={styles['app-header']}>
                 {/* top-header */}
                 <div className={styles['top-header']}>
                     <div className={styles.inner}>
-                        <Typography.Text>让旅游更幸福</Typography.Text>
+                        <Typography.Text>{t("header.slogan")}</Typography.Text>
                         <Dropdown.Button
                             style={{ marginLeft: 15 }}
                             overlay={
@@ -68,15 +69,15 @@ export class HeaderComponent extends React.Component<Props, State> {
                             {this.state.language === "zh" ? "中文" : "English"}
                         </Dropdown.Button>
                         <Button.Group className={styles['botton-group']}>
-                            <Button onClick={() => history.push('register')}>注册</Button>
-                            <Button onClick={() => history.push('signIn')}>登录</Button>
+                            <Button onClick={() => history.push('register')}>{t("header.register")}</Button>
+                            <Button onClick={() => history.push('signIn')}>{t("header.signin")}</Button>
                         </Button.Group>
                     </div>
                 </div>
                 <Layout.Header className={styles['main-header']}>
                     <span onClick={() => history.push('/')}>
                         <img src={logo} alt="logo" className={styles['App-logo']} />
-                        <Typography.Title level={3} className={styles.title}>React 旅游网</Typography.Title>
+                        <Typography.Title level={3} className={styles.title}></Typography.Title>
                     </span>
                
                     <Input.Search className={styles['search-input']}
@@ -84,27 +85,27 @@ export class HeaderComponent extends React.Component<Props, State> {
                     />
                 </Layout.Header>
                 <Menu mode={"horizontal"} className={styles['main-menu']}>
-                    <Menu.Item key={1}>旅游首页</Menu.Item>
-                    <Menu.Item key={2}>周末游</Menu.Item>
-                    <Menu.Item key={3}>跟团游</Menu.Item>
-                    <Menu.Item key={4}>自由行</Menu.Item>
-                    <Menu.Item key={5}>私家团</Menu.Item>
-                    <Menu.Item key={6}>游轮</Menu.Item>
-                    <Menu.Item key={7}>酒店+景点</Menu.Item>
-                    <Menu.Item key={8}>当地玩乐</Menu.Item>
-                    <Menu.Item key={9}>主题游</Menu.Item>
-                    <Menu.Item key={10}>定制游</Menu.Item>
-                    <Menu.Item key={11}>游学</Menu.Item>
-                    <Menu.Item key={12}>签证</Menu.Item>
-                    <Menu.Item key={13}>企业游</Menu.Item>
-                    <Menu.Item key={14}>高端游</Menu.Item>
-                    <Menu.Item key={15}>爱玩户外</Menu.Item>
-                    <Menu.Item key={16}>保险</Menu.Item>
+                    <Menu.Item key="1">{t("header.home_page")}</Menu.Item>
+                    <Menu.Item key="2">{t("header.weekend")}</Menu.Item>
+                    <Menu.Item key="3">{t("header.group")}</Menu.Item>
+                    <Menu.Item key="4">{t("header.backpack")}</Menu.Item>
+                    <Menu.Item key="5">{t("header.private")}</Menu.Item>
+                    <Menu.Item key="6">{t("header.cruise")}</Menu.Item>
+                    <Menu.Item key="7">{t("header.hotel")}</Menu.Item>
+                    <Menu.Item key="8">{t("header.local")}</Menu.Item>
+                    <Menu.Item key="9">{t("header.theme")}</Menu.Item>
+                    <Menu.Item key="10">{t("header.custom")}</Menu.Item>
+                    <Menu.Item key="11">{t("header.study")}</Menu.Item>
+                    <Menu.Item key="12">{t("header.visa")}</Menu.Item>
+                    <Menu.Item key="13">{t("header.enterprise")}</Menu.Item>
+                    <Menu.Item key="14">{t("header.high_end")}</Menu.Item>
+                    <Menu.Item key="15">{t("header.outdoor")}</Menu.Item>
+                    <Menu.Item key="16">{t("header.insurance")}</Menu.Item>
                 </Menu>
             </div>
         )
     }   
 }
 
-export const Header =  withRouter(HeaderComponent);
+export const Header =  withTranslation()(withRouter(HeaderComponent));
 
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Header, Footer, Carousel, SideMenu, ProductCollection } from "../../components";
 import { Row, Col, Typography } from 'antd';
+import { withTranslation, WithTranslation } from 'react-i18next'
 import { productList1, productList2, productList3 } from "./mockups";
 import sideImage from "../../assets/images/sider_2019_12-09.png";
 import sideImage2 from "../../assets/images/sider_2019_02-04.png";
 import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
+import { Header, Footer, Carousel, SideMenu, ProductCollection } from "../../components";
+
 import styles from './HomePage.module.css'
 
-export class HomePage extends React.Component {
+interface IHomePageProps {} 
+
+type Props = IHomePageProps & WithTranslation;
+
+class HomePageComponet extends React.Component<Props> {
     render() {
+        const { t } = this.props;
         return <> 
         <Header />
         {/* 页面的内容 content */}
@@ -22,17 +29,17 @@ export class HomePage extends React.Component {
                 </Col>
             </Row>
             <ProductCollection
-                title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
+                title={<Typography.Title level={3} type="warning">{t("home_page.hot_recommended")}</Typography.Title>}
                 sideImage={sideImage}
                 products={productList1}
             />
              <ProductCollection
-                title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
+                title={<Typography.Title level={3} type="danger">{t("home_page.new_arrival")}</Typography.Title>}
                 sideImage={sideImage2}
                 products={productList2}
             />
             <ProductCollection
-                title={<Typography.Title level={3} type="success">新品上市</Typography.Title>}
+                title={<Typography.Title level={3} type="success">{t("home_page.domestic_travel")}</Typography.Title>}
                 sideImage={sideImage3}
                 products={productList3}
             />
@@ -41,3 +48,6 @@ export class HomePage extends React.Component {
         </>
     }
 }
+
+// 第一个小括号代表的是语言所使用的命名空间
+export const HomePage = withTranslation()(HomePageComponet);
