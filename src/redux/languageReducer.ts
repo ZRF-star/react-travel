6abdfa00651b556e05a160b0a542e562ede326f0
ZRF-star <zhangrf136@nenu.edu.cn>
2021-10-18 11:31:07 +0800

    refactor: i18n 实现中英文切换（这里reducer不是纯函数，待定修改）

--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -1,3 +1,5 @@
+import i18n from 'i18next'
+
 export interface LanguageState {
     language:"en" | "zh",
     languageList:{name:string, code:string}[]
@@ -15,6 +17,7 @@ export default (state = defaultState, action) => {
     let newState;
     switch(action.type) {
         case "change_language":
+            i18n.changeLanguage(action.payload);
            newState = {...state, language:action.payload};
             return newState;
         case "add_language":
